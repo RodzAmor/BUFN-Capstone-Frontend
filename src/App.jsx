3dccac18bb498d14709b7acfd8f2f3f60bc44838
Rodz Andrie Amor <ramorstem2022@example.com>
2024-04-13 16:41:15 -0400

    Ready to move to Prod

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,20 @@
 import { useState, useEffect } from 'react';
 import './index.css';
-// import * as tf from '@tensorflow/tfjs';
 
 
-// let model;
-
-// const loadModel = async () => {
-//   const use = require('@tensorflow-models/universal-sentence-encoder');
-//   // model = await use.load();
-//   model = await use.loadLite();
-//   console.log('Model loaded');
-//   return model;
-// };
-
-// const encodeText = async (model, texts) => {
-//   return await model.embed(texts);
-// };
-
-
-// const cosineSimilarity = (a, b) => {
-//   console.log('Shape of a:', a.shape);  // Debugging the shape of tensor 'a'
-//   console.log('Shape of b:', b.shape);  // Debugging the shape of tensor 'b'
-
-//   // Expand 'a' to 2D if it is 1D and normalize
-//   const a2D = a.shape.length === 1 ? a.expandDims(0) : a;
-//   const normA = tf.norm(a2D, 'euclidean', 1, true);  // Keepdims to maintain 2D shape
-//   const normalizedA = a2D.div(normA);
-
-//   // Normalize 'b' and ensure it remains 2D
-//   const normB = tf.norm(b, 'euclidean', 1, true);  // Keepdims to maintain shape compatibility
-//   const normalizedB = b.div(normB);
-
-//   // Matrix multiplication between 2D tensors
-//   return tf.matMul(normalizedA, normalizedB, false, true);
-// };
-
-
-// // Main function to analyze text
-// const analyzeText = async (sentence, largeText) => {
-//   const model = await loadModel();
-//   const segments = largeText.match(/[\s\S]{1,500}(?:\s|$)/g) || []; // Regex to split text into segments of up to 500 characters
-
-//   // Encode the sentence and all segments
-//   const sentenceEmbedding = await encodeText(model, [sentence]);
-//   const segmentEmbeddings = await encodeText(model, segments);
-
-//   // Calculate similarities
-//   const similarities = cosineSimilarity(sentenceEmbedding.squeeze(), segmentEmbeddings).dataSync();
-  
-//   // Find the highest similarity and corresponding segment
-//   let highestSimilarity = -1;
-//   let bestSegment = "";
-//   for (let i = 0; i < similarities.length; i++) {
-//       if (similarities[i] > highestSimilarity) {
-//           highestSimilarity = similarities[i];
-//           bestSegment = segments[i];
-//       }
-//   }
-
-//   return {
-//       highestSimilarity: highestSimilarity,
-//       bestSegment: bestSegment
-//   };
-// };
 
 function App() {
+  const [exampleHeadlines, setExampleHeadlines] = useState([
+  "U.S. Approves $1.5 Billion Loan to Restart Michigan Nuclear Plant",
+  "Biden Plans Sweeping Effort to Block Arctic Oil Drilling",
+  "Jobs Report Looms What's Next for Energy Transfer Stock and Its 8% Dividend Yield?",
+  "IEA Expects Global Oil-Demand Growth to Slow Further in 2025",
+  "Environmentalists protest as Biden administration approves huge oil export terminal off Texas coast",,
+  "Driven by China, Coal Plants Made a Comeback in 2023",
+  "Energy Dept. Awards $6 Billion for Green Steel, Cement and Even Macaroni Factories",
+  "U.S. Seeks to Boost Nuclear Power After Decades of Inertia",
+  "With inflation stalling, the long-predicted storm clouds in the economy may actually be forming"]);
+
   const [year, setYear] = useState(2024);
   const [tableData, setTableData] = useState([]);
   const [error, setError] = useState('');
@@ -72,17 +22,14 @@ function App() {
   const [isLoading, setIsLoading] = useState(false); // State to track loading status
   const [isAnalyzing, setIsAnalyzing] = useState(false); // State to track loading status
   const [analyzingCompany, setAnalyzingCompany] = useState(''); // State to track loading status
-
-  // useEffect(() => {
-  //   loadModel(); // Load the model on component mount
-  // }, []);
-
+  const environment = "prod"
+  
   const fetchExampleData = (e) => {
     e.preventDefault(); // Prevent default form submission behavior
     setIsLoading(true); // Start loading
 
-    // fetch(`http://127.0.0.1:5000/example`)
-    fetch(`http://127.0.0.1:5000/example`)
+    const fetchEndpoint = environment === "dev" ? `http://127.0.0.1:5000/example` : "https://finance-risk-toolkit-api-scx3vdzzxa-ue.a.run.app/example";
+    fetch(fetchEndpoint)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -101,6 +48,32 @@ function App() {
     .finally(() => setIsLoading(false));
   }
   
+  useEffect(() => {
+    fetchHeadlines();
+  }, []);
+
+  const fetchHeadlines = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch('https://gnews.io/api/v4/top-headlines?q=energy+sector&category=business&token=ae289a4a0a662da6d2aee99dfe8a9a43&lang=en&country=us');
+      // const response = "";
+      if (!response.ok) {
+        throw new Error('Failed to fetch headlines');
+      }
+      const data = await response.json();
+      setExampleHeadlines(data.articles.map(article => article.title));
+      data.articles.forEach(article => {
+        console.log(article.title)
+      });
+    } catch (error) {
+      // setError('Failed to load headlines. API key has run out of available calls.');
+      // console.error('API key has run out of available calls. Error fetching headlines:', error);
+      // console.log("It will be prefilled with data.")
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   const fetchData = async () => {
     setTableData([]);
     if (!headline) {
@@ -112,31 +85,27 @@ function App() {
     setIsLoading(true);
     setError('');
 
-    const delay = ms => new Promise(res => setTimeout(res, ms));
-
-
-    const ticker = ['XOM', "PXD", "ETR", "ED", "PCG"];
+    const ticker = ["XOM", "PXD", "ETR", "ED", "PCG", "OSG"];
     try {
       for (const tickerSymbol of ticker) {
         setAnalyzingCompany(tickerSymbol);
         setIsAnalyzing(true);
-
-        const response = await fetch(`http://127.0.0.1:5000/analyze-company?ticker=${tickerSymbol}&year=${year}&headline=${headline}`);
+        const fetchEndpoint = environment === "dev" ? `http://127.0.0.1:5000/analyze-company?ticker=${tickerSymbol}&year=${year}&headline=${headline}` 
+        : `https://finance-risk-toolkit-api-scx3vdzzxa-ue.a.run.app/analyze-company?ticker=${tickerSymbol}&year=${year}&headline=${headline}`;
+        const response = await fetch(fetchEndpoint);
         if (!response.ok) {
           throw new Error(`Failed to fetch data for ticker ${tickerSymbol}`);
         }
         const data = await response.json();
-
-
+        
+        
         const newEntry = {
           Ticker: data[0]["Ticker"],
-          CompanyName: data[0]["Company Name"],
-          FillDate: data[0]["Fill Date"],
+          "Company Name": data[0]["Company Name"],
+          "Fill Date": data[0]["Fill Date"],
           Year: data[0]["Year"],
-          Headline: headline,
-          // "Highest Semantic Similarity Score": result.highestSimilarity,
-          // "Risk Section Representative Segment": result.bestSegment,
-          "Highest Semantic Similarity Score": data[0]["Highest Similarity Score"],
+          "Headline": headline,
+          "Highest Semantic Similarity": data[0]["Highest Similarity Score"],
           "Risk Section Representative Segment": data[0]["Risk Section Representative Segment"],
         };
         setTableData(prevData => [...prevData, newEntry]);
@@ -147,7 +116,7 @@ function App() {
       console.error('Error:', error);
       setError(`Error: ${error.message}`);
     } finally {
-      setIsLoading(false);
+      // setIsLoading(false);
     }
   };
 
@@ -167,29 +136,44 @@ function App() {
       <div className="container text-center mx-auto">
         <h1 className="text-xl font-semibold p-4 mb-2">Event Semantic Similarity Analysis With Company SEC 10-K Risk Factors</h1>
         <p className='mb-6'>
-          Enter a news headline or label, representing an <b>event</b>. <br></br>
-          This toolkit will analyze if a company has listed concerns related to the event in the risk section of their SEC 10-K.
+          This toolkit will analyze if a company has listed concerns related to the event in the risk section of their SEC 10-K. <br></br>
+          Enter a news headline or label representing an <b>event</b> or select a top news headline below:
         </p>
         {/* Form for ticker and year input */}
-        <form className="mb-4">
-          <input 
-            type="text" 
-            placeholder="Enter Headline Here" 
-            className="input input-bordered w-full max-w-xs mr-2"
-            value={headline}
-            onChange={(e) => setHeadline(e.target.value.toUpperCase())} // Convert ticker to uppercase
-          />
-          <input 
-            type="number" 
-            placeholder="Enter Year Here" 
-            className="input input-bordered w-24 max-w-xs mr-2"
-            value={year}
-            onChange={(e) => setYear(e.target.value)}
-          />
-          <button type="button" className="btn btn-primary" onClick={fetchData}>Analyze Headline</button>
+        <form className="mb-4 flex items-center mx-auto" >
+          <div className="flex flex-col items-end w-1/2 p-4">
+            <select 
+                className="select select-bordered mb-2 w-96" 
+                value={headline}
+                onChange={(e) => setHeadline(e.target.value)}
+              >
+                <option value="">Select a current news headline or type one below</option>
+                {exampleHeadlines.map((line, index) => (
+                  <option key={index} value={line}>{line}</option>
+                ))}
+            </select>
+            <textarea 
+              type="text" 
+              placeholder="Enter Headline Here" 
+              className="input input-bordered h-24 w-96" 
+              value={headline}
+              onChange={(e) => setHeadline(e.target.value)} // Convert ticker to uppercase
+            />
+          </div>
+          <div className="flex flex-col align-middle w-1/2 p-4">
+            <p className='inline w-48'>Annual Report Year</p>
+            <input 
+              type="number" 
+              placeholder="Enter Year Here" 
+              className="input input-bordered w-48 mt-2 mb-4"
+              value={year}
+              onChange={(e) => setYear(e.target.value)}
+            />
+            <button type="button" className="btn btn-primary w-48" onClick={fetchData}>Analyze Headline</button>
+          </div>
         </form>
         <form onSubmit={fetchExampleData} className="mb-4">
-          <button type="submit" className="btn btn-secondary">Show Example Headline</button>
+          <button type="submit" className="btn btn-secondary">Show Example Output</button>
         </form>
 
         {!isLoading && tableData.length > 0 && (
@@ -212,11 +196,12 @@ function App() {
               ))}
             </tbody>
           </table>
-          {isAnalyzing && <p>Analyzing: {analyzingCompany}...</p>}
+          {isAnalyzing && <p className='m-2'>Analyzing: {analyzingCompany}...</p>}
         </div>
         )}
 
-      {isLoading && <p>Loading...</p>}
+      
+      {isLoading && <p>Processing Headline...</p>}
       {error && <p className="text-red-500">{error}</p>}
       
 
@@ -226,5 +211,14 @@ function App() {
     </div>
   );
 }
-
 export default App;
+
+
+// docker build -t finance-toolkit-api .
+// docker buildx build --platform linux/amd64 -t finance-toolkit-api .
+// docker tag finance-toolkit-api gcr.io/bufn-capstone/finance-toolkit-api:latest
+// docker push gcr.io/bufn-capstone/finance-toolkit-api:latest
+// gcloud run deploy finance-toolkit-api --image gcr.io/bufn-capstone/finance-toolkit-api:latest --platform managed --allow-unauthenticated
+
+// docker run --rm -p 8080:8080 finance-toolkit-api
+// docker image inspect finance-toolkit-api
