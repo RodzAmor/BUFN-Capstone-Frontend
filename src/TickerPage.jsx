1bdb97a51f6105bd8743700c7c6e15a9a99f17aa
Rodz Andrie Amor <ramorstem2022@example.com>
2024-04-23 01:35:34 -0400

    A few fixes and ready for prod

--- src/TickerPage.jsx
+++ src/TickerPage.jsx
@@ -12,12 +12,14 @@ const TickerPage = () => {
 
     // Assume that the year is passed as state in the navigate function from App.jsx
     const selectedYear = location.state?.year; 
+    const environment = "prod"
+    const apiUrl = environment === "dev" ? "http://127.0.0.1:5000" : "https://finance-risk-toolkit-api-scx3vdzzxa-ue.a.run.app";
 
     useEffect(() => {
         const fetchStockData = async () => {
             try {
                 // Use the selected year in the API call
-                const response = await fetch(`/api/stock/${ticker}?year=${selectedYear}`);
+                const response = await fetch(`${apiUrl}/api/stock/${ticker}?year=${selectedYear}`);
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
@@ -60,31 +62,34 @@ const TickerPage = () => {
     return (
         <div>
             <div className="navbar bg-slate-200 p-3 mb-2">
-            <div className="navbar-start">
-            </div>
-            <div className="navbar-center">
-                <a href="" className="btn btn-ghost normal-case font-extrabold text-xl sm:text-3xl">Financial Event Risk Analysis Tool</a>
-            </div>
-            <div className="navbar-end">
-            </div>
+                <div className="navbar-start">
+
+                </div>
+                <div className="navbar-center">
+                    <a href="../" className="btn btn-ghost normal-case font-extrabold text-xl sm:text-3xl">Financial Event Risk Analysis Tool</a>
+                </div>
+                <div className="navbar-end">
+                    
+                </div>
             </div>
             <div className="flex flex-col items-center justify-center min-h-screen">
-            <h1 className="text-3xl font-bold mb-6 text-center">{ticker} Information for {selectedYear}</h1>
-            {loading && <p>Loading...</p>}
-            {error && <p className="text-red-500 text-center">{error}</p>}
-            {!loading && !error && stockData && (
-                <div className="w-full max-w-4xl mx-auto"> {/* Adjust the max-width as needed */}
-                    <div className="w-full" style={{ height: '50vh' }}> {/* Adjust the height as needed */}
-                        <Line 
-                            data={stockData} 
-                            options={{
-                                maintainAspectRatio: false, // Change to false to allow custom height
-                                responsive: true,
-                            }}
-                        />
+            <a href="../" className="btn normal-case font-extrabold text-xl sm:text-3xl mb-12">Back to Toolkit</a>
+                <h1 className="text-3xl font-bold mb-6 text-center">{ticker} Information for {selectedYear}</h1>
+                {loading && <p>Loading...</p>}
+                {error && <p className="text-red-500 text-center">{error}</p>}
+                {!loading && !error && stockData && (
+                    <div className="w-full max-w-4xl mx-auto"> {/* Adjust the max-width as needed */}
+                        <div className="w-full" style={{ height: '50vh' }}> {/* Adjust the height as needed */}
+                            <Line 
+                                data={stockData} 
+                                options={{
+                                    maintainAspectRatio: false, // Change to false to allow custom height
+                                    responsive: true,
+                                }}
+                            />
+                        </div>
                     </div>
-                </div>
-            )}
+                )}
             </div>
         </div>
     );
