ae57d1a613cb5b7338e439f7a0056487a84230e2
Rodz Andrie Amor <ramorstem2022@example.com>
2024-04-23 00:48:47 -0400

    Added filter by ff5 sector to the website

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './index.css';
 
-
-
 function App() {
   const navigate = useNavigate();
   const [selectedRowIndex, setSelectedRowIndex] = useState(null); // State to track the selected row index
-  // Other states and functions...
 
   const handleRowClick = (index, ticker) => {
     setSelectedRowIndex(index); // Set the selected row index
@@ -28,18 +25,35 @@ function App() {
   const [year, setYear] = useState(2024);
   const [tableData, setTableData] = useState([]);
   const [error, setError] = useState('');
+  const [abortHelper, setAbortHelper] = useState(false);
   const [headline, setHeadline] = useState('');
+  const [limit, setLimit] = useState('');
+  const [tickers, setTickers] = useState([]);
+  const [sector, setSector] = useState(0);
   const [isLoading, setIsLoading] = useState(false); // State to track loading status
   const [isAnalyzing, setIsAnalyzing] = useState(false); // State to track loading status
   const [analyzingCompany, setAnalyzingCompany] = useState(''); // State to track loading status
+  const [abortController, setAbortController] = useState(null);
+
+
   const environment = "dev"
+  const apiUrl = environment === "dev" ? "http://127.0.0.1:5000" : "https://finance-risk-toolkit-api-scx3vdzzxa-ue.a.run.app";
 
   const fetchExampleData = (e) => {
     e.preventDefault(); // Prevent default form submission behavior
+    
+    if (abortController) {
+      console.log("Abort!");
+      abortController.abort(); // Abort any ongoing fetch operation
+    }
+    setAbortHelper(true);
+    console.log("abort helper? " + abortHelper)
+
+    const newAbortController = new AbortController();
+    setAbortController(newAbortController);
     setIsLoading(true); // Start loading
 
-
-    const fetchEndpoint = environment === "dev" ? `http://127.0.0.1:5000/example` : "https://finance-risk-toolkit-api-scx3vdzzxa-ue.a.run.app/example";
+    const fetchEndpoint = `${apiUrl}/example`;
     fetch(fetchEndpoint)
     .then(response => {
       if (!response.ok) {
@@ -56,15 +70,24 @@ function App() {
       console.error('Error fetching data:', error);
       setError('Failed to fetch data. Please try again later.');
     })
-    .finally(() => setIsLoading(false));
+    .finally(() => {setIsLoading(false); setIsAnalyzing(false);});
   }
   
   useEffect(() => {
     fetchHeadlines();
+    fetchTickers();
   }, []);
 
   const fetchHeadlines = async () => {
     setIsLoading(true);
+
+    if (abortController) {
+      abortController.abort(); // Abort any ongoing fetch operation
+    }
+  
+    const newAbortController = new AbortController();
+    setAbortController(newAbortController);
+
     try {
       const response = await fetch('https://gnews.io/api/v4/top-headlines?q=energy+sector&category=business&token=ae289a4a0a662da6d2aee99dfe8a9a43&lang=en&country=us');
       // const response = "";
@@ -85,7 +108,52 @@ function App() {
     }
   };
 
+  const getBackgroundColor = (key, value) => {
+    if (key === 'Highest Semantic Similarity') {
+      const sentiment = parseFloat(value);
+      if (!isNaN(sentiment)) {
+        if (sentiment > 0.3) {
+          return '#b5ffad';
+        } else if (sentiment > 0.2) {
+          return '#fffaad'; // Slightly darker yellow
+        } else {
+          return '#edb4b4';
+        }
+      }
+    }
+    return 'inherit'; // Default background color
+  };
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    }
+  }
+
+  const fetchTickers = async () => {
+    try {
+      const fetchEndpoint = `${apiUrl}/get-tickers?farma=${sector}`
+      const response = await fetch(fetchEndpoint);
+      const data = await response.json();
+      
+      console.log(data["tickers"]);
+      shuffleArray(data["tickers"]); // Shuffle the array
+      setTickers(data["tickers"]);
+    } catch (e) {
+      setTickers([]);
+    }
+  };
+
   const fetchData = async () => {
+    setAbortHelper(false);
+    if (abortController) {
+      abortController.abort(); // Abort any ongoing fetch operation
+    }
+
+    const newAbortController = new AbortController();
+    setAbortController(newAbortController);
+
     setTableData([]);
     if (!headline) {
       setError('Headline is required');
@@ -96,19 +164,18 @@ function App() {
     setIsLoading(true);
     setError('');
 
-    const ticker = ["XOM", "PXD", "ETR", "ED", "PCG", "OSG"];
-    try {
-      for (const tickerSymbol of ticker) {
+    for (const tickerSymbol of tickers) {
+        try {
         setAnalyzingCompany(tickerSymbol);
         setIsAnalyzing(true);
-        const fetchEndpoint = environment === "dev" ? `http://127.0.0.1:5000/analyze-company?ticker=${tickerSymbol}&year=${year}&headline=${headline}` 
-        : `https://finance-risk-toolkit-api-scx3vdzzxa-ue.a.run.app/analyze-company?ticker=${tickerSymbol}&year=${year}&headline=${headline}`;
-        const response = await fetch(fetchEndpoint);
+        const fetchEndpoint = `${apiUrl}/analyze-company?ticker=${tickerSymbol}&year=${year}&headline=${headline}`;
+
+        const response = await fetch(fetchEndpoint, { signal: newAbortController.signal });
         if (!response.ok) {
           throw new Error(`Failed to fetch data for ticker ${tickerSymbol}`);
         }
+
         const data = await response.json();
-        
         
         const newEntry = {
           Ticker: data[0]["Ticker"],
@@ -119,16 +186,20 @@ function App() {
           "Highest Semantic Similarity": data[0]["Highest Similarity Score"],
           "Risk Section Representative Segment": data[0]["Risk Section Representative Segment"],
         };
+        if (data[0]["Ticker"] == null) {
+            continue;
+        }
         setTableData(prevData => [...prevData, newEntry]);
         setIsLoading(false);
         setIsAnalyzing(false);
+
+        } catch (error) {
+          console.error('Error:', error);
+          // setError(`Error: ${error.message}`);
+        } finally {
+          // setIsLoading(false);
+        }
       }
-    } catch (error) {
-      console.error('Error:', error);
-      setError(`Error: ${error.message}`);
-    } finally {
-      // setIsLoading(false);
-    }
   };
 
 
@@ -172,24 +243,6 @@ function App() {
     return csvRows.join('\n');
   };
 
-  const getBackgroundColor = (key, value) => {
-    if (key === 'Highest Semantic Similarity') {
-      const sentiment = parseFloat(value);
-      if (!isNaN(sentiment)) {
-        if (sentiment > 0.3) {
-          return '#b5ffad';
-        } else if (sentiment > 0.2) {
-          return '#fffaad'; // Slightly darker yellow
-        } else {
-          return '#edb4b4';
-        }
-      }
-    }
-    return 'inherit'; // Default background color
-  };
-  
-  
-  
   
   return (
     <div className="App">
@@ -210,8 +263,8 @@ function App() {
           Enter a news headline or label representing an <b>event</b> or select a top news headline below:
         </p>
         {/* Form for ticker and year input */}
-        <form className="mb-4 flex flex-col sm:flex-row items-center mx-auto" >
-          <div className="flex flex-col items-center sm:items-end w-1/2 p-4">
+        <form className="mb-4 flex flex-col sm:flex-row justify-center items-center mx-auto" >
+          <div className="flex flex-col items-center sm:items-end w-1/3 p-4">
             <select 
                 className="select select-bordered mb-2 w-96" 
                 value={headline}
@@ -230,7 +283,8 @@ function App() {
               onChange={(e) => setHeadline(e.target.value)} // Convert ticker to uppercase
             />
           </div>
-          <div className="flex flex-col items-center sm:items-start align-middle w-1/2 p-4">
+          
+          <div className="flex flex-col items-center sm:items-start align-middle w-1/3 p-4">
             <p className='inline w-48'>Annual Report Year</p>
             <input 
               type="number" 
@@ -239,6 +293,20 @@ function App() {
               value={year}
               onChange={(e) => setYear(e.target.value)}
             />
+            <p className='inline w-52 mb-2'>Select Farma French Sector</p>
+            <select 
+                className="select select-bordered mb-2" 
+                value={sector}
+                onChange={(e) => {setSector(e.target.value); fetchTickers();}}
+              >
+                <option key="0" value="0">All Sectors</option>
+                <option key="1" value="1">Consumer</option>
+                <option key="2" value="2">Manufacturing</option>
+                <option key="3" value="3">HiTec</option>
+                <option key="4" value="4">Health and Medical</option>
+                <option key="5" value="5">Energy</option>
+                <option key="6" value="6">Other including Finance</option>
+            </select>
             <button type="button" className="btn btn-primary w-48" onClick={fetchData}>Analyze Headline</button>
           </div>
         </form>
@@ -257,16 +325,16 @@ function App() {
                 </tr>
               </thead>
               <tbody>
-              {tableData.map((row, index) => (
-                <tr key={index} 
-                    onClick={() => handleRowClick(index, row['Ticker'])}
-                    className={selectedRowIndex === index ? "selected-row" : ""}
-                >
-                  {Object.entries(row).map(([key, value], valueIndex) => (
-                    <td key={valueIndex} style={{ backgroundColor: getBackgroundColor(key, value) }}>{value}</td>
-                  ))}
-                </tr>
-              ))}
+                {tableData.map((row, index) => (
+                  <tr key={index} 
+                      onClick={() => handleRowClick(index, row['Ticker'])}
+                      className={selectedRowIndex === index ? "selected-row" : ""}
+                  >
+                    {Object.entries(row).map(([key, value], valueIndex) => (
+                      <td key={valueIndex} style={{ backgroundColor: getBackgroundColor(key, value) }}>{value}</td>
+                    ))}
+                  </tr>
+                ))}
               </tbody>
             </table>
             {isAnalyzing && <p className='m-2'>Analyzing: {analyzingCompany}...</p>}
